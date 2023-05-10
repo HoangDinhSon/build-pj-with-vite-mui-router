e413a9e6f614e777ed6b277474690d59926bd09d
HoangDinhSon <dinhson040494@gmail.com>
2023-05-10 12:03:04 +0700

    second

--- buil-react-with-vite/src/App.tsx
+++ buil-react-with-vite/src/App.tsx
@@ -1,12 +1,16 @@
-
-
+import Button from "@mui/material/Button";
+import * as React from "react";
+import {TextField} from "@mui/material"
+import {Avatar} from "@mui/material"
 
 function App() {
-
+ const  refButton =React.createRef();
   return (
-    <>
-      <h1 className="underline">hello</h1>
-    </>
+    <div>
+      <TextField variant="outlined" label="Name" multiline={false}></TextField>
+      <Avatar src="https://mui.com/static/images/avatar/1.jpg" ></Avatar>
+      <h1 className="underline">Hello</h1>
+    </div>
   )
 }
 
